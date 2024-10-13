858560187906f6ba8c711554f9f6a7ab32214949
hitsan <soledewa2828@gmail.com>
2024-10-13 15:27:26 +0000

    Add user method

--- src/user/user.rs
+++ src/user/user.rs
@@ -1,63 +1,54 @@
 use crate::card::card::Card;
 
 #[derive(Debug, PartialEq)]
-struct Player {
+struct WaitingPlayer {
     name: String,
-    cards: Cards
+    card: Card
 }
 
-enum Picked {
-    Hand,
-    Drawn,
-}
-
-#[derive(Debug, PartialEq)]
-enum Cards {
-    Single(Card),
-    Double{ hand: Card, drawn: Card },
-}
-
-impl Player {
+impl WaitingPlayer {
     fn new(name: String, card: Card) -> Self {
-        let cards = Cards::Single(card);
-        Self{name, cards}
+        Self{name, card}
     }
 
-    fn draw(&self, card: Card) -> Self {
-        if let Cards::Single(hand) = self.cards {
-            let cards = Cards::Double{ hand, drawn: card};
-            let name = self.name.clone();
-            return Self{name, cards}
-        }
-        panic!("Illegal player method")
-    }
-
-    fn open(&self, picked: Picked) -> Card {
-        let (hand, drawn) = match self.cards {
-            Cards::Double{hand, drawn} => (hand, drawn),
-            _ => panic!("Invalid number of cards!")
-        };
-        match picked {
-            Picked::Hand => hand,
-            Picked::Drawn => drawn,
-        }
-    }
-
-    fn drop(&self, picked: Picked) -> Self {
-        let (hand, drawn) = match self.cards {
-            Cards::Double{hand, drawn} => (hand, drawn),
-            _ => panic!("Invalid number of cards!")
-        };
+    fn draw(&self, card: Card) -> CurrentPlayer {
         let name = self.name.clone();
-        match picked {
-            Picked::Hand => Self::new(name, drawn),
-            Picked::Drawn => Self::new(name, hand),
-        }
+        let hand = self.card;
+        CurrentPlayer::new(name, hand, card)
+    }
+
+    fn open(&self) -> &Card {
+        &self.card
     }
 
     fn loose(&self) -> Looser {
         Looser::new(self.name.clone())
     }
+}
+
+#[derive(Debug, PartialEq)]
+struct CurrentPlayer {
+    name: String,
+    hand: Card,
+    drawn: Card,
+}
+
+impl CurrentPlayer {
+    fn new(name: String, hand: Card, drawn: Card) -> Self {
+        Self{name, hand, drawn}
+    }
+
+    fn drop(&self, picked: Picked) -> WaitingPlayer {
+        let name = self.name.clone();
+        let card = if picked == Picked::Hand { self. drawn } else { self.hand };
+        WaitingPlayer::new(name, card)
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum Picked {
+    Hand,
+    Drawn
 }
 
 #[derive(Debug, PartialEq)]
@@ -79,7 +70,7 @@ mod tests {
     #[test]
     fn test_loose() {
         let name = String::from("player1");
-        let player = Player::new(name, Princess);
+        let player = WaitingPlayer::new(name, Princess);
         let looser = player.loose();
 
         let name = String::from("player1");
@@ -90,47 +81,37 @@ mod tests {
     #[test]
     fn test_draw() {
         let name = String::from("player1");
-        let player = Player::new(name, Princess);
+        let player = WaitingPlayer::new(name, Princess);
         let player = player.draw(Minister);
 
         let name = String::from("player1");
-        let cards = Cards::Double {hand: Princess, drawn: Minister};
-        assert_eq!(player, Player{name, cards});
+        assert_eq!(player, CurrentPlayer::new(name, Princess, Minister));
     }
 
     #[test]
     fn test_open() {
         let name = String::from("player1");
-        let cards = Cards::Double {hand: Princess, drawn: Minister};
-        let player = Player { name, cards };
-        let card = player.open(Picked::Hand);
-        assert_eq!(card, Card::Princess);
-
+        let player = WaitingPlayer::new(name, Princess);
+        let card = player.open();
+        assert_eq!(card, &Card::Princess);
         let name = String::from("player1");
-        let cards = Cards::Double {hand: Princess, drawn: Minister};
-        let player = Player { name, cards };
-        let card = player.open(Picked::Drawn);
-        assert_eq!(card, Card::Minister);
+        assert_eq!(player, WaitingPlayer::new(name, Princess));
     }
 
     #[test]
     fn test_drop() {
         let name = String::from("player1");
-        let cards = Cards::Double {hand: Princess, drawn: Minister};
-        let player = Player { name, cards };
+        let player = CurrentPlayer::new(name, Princess, Knight);
         let player = player.drop(Picked::Hand);
         let name = String::from("player1");
-        let cards = Cards::Single(Minister);
-        let expected = Player { name, cards };
+        let expected = WaitingPlayer::new(name, Knight);
         assert_eq!(player, expected);
 
         let name = String::from("player1");
-        let cards = Cards::Double {hand: Princess, drawn: Minister};
-        let player = Player { name, cards };
+        let player = CurrentPlayer::new(name, Princess, Knight);
         let player = player.drop(Picked::Drawn);
         let name = String::from("player1");
-        let cards = Cards::Single(Princess);
-        let expected = Player { name, cards };
+        let expected = WaitingPlayer::new(name, Princess);
         assert_eq!(player, expected);
     }
 
